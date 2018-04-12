08929933f42e69023766303d24840a993b5a8a11
moon <moon@moondeMacBook-Pro.local>
2018-04-12 19:13:16 +0800

    1

---
+++ autotrade/src/main/java/com/meetcity/huobi/api/Constants.java
@@ -0,0 +1,12 @@
+package com.meetcity.huobi.api;
+
+/**
+ * Created by moon on 2018/4/12 .
+ *
+ * 常量
+ */
+public class Constants {
+
+    public static String base_url = "api.huobi.pro";
+    public static String AccessKeyId = "";
+}
